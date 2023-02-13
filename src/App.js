a492e94d73d48113e5d4356921d1d56077d26419
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-02-13 18:12:41 +0545

    starter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useQuery } from "react-query";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  const fetchPosts = async () => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_page=2&_limit=10`
+    );
+    return response.json();
+  };
+
+  const { data, isSuccess } = useQuery("repos", fetchPosts);
+
+  return (
+    <div className="app">
+      {isSuccess &&
+        data.map((post) => (
+          <div className="article" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default App;
