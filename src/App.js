3038a00f070156e4467e681783023f2556eb60a1
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-02-16 21:12:01 +0545

    final changes for starter

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import { useQuery } from "react-query";
 import "./App.css";
 
 function App() {
-  const fetchPosts = async () => {
+  const fetchTodos = async () => {
     const response = await fetch(
-      `https://jsonplaceholder.typicode.com/posts?_page=2&_limit=10`
+      `https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10`
     );
     return response.json();
   };
 
-  const { data, isSuccess } = useQuery("repos", fetchPosts);
+  const { data, isSuccess } = useQuery("todos", fetchTodos);
 
   return (
     <div className="app">
       {isSuccess &&
-        data.map((post) => (
-          <div className="article" key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+        data.map((todo) => (
+          <div className="article" key={todo.id}>
+            <h2>{todo.title}</h2>
+            <p>Status: {todo.completed ? "Completed" : "To Complete"}</p>
           </div>
         ))}
     </div>
